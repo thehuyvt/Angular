31b2a2077389ddbe8b8258fa55016d2286b18d76
thehuyvt <thehuyvtvp@gmail.com>
2024-01-21 09:08:19 +0700

    DataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
-
+import { FormsModule } from '@angular/forms';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HomeComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent, FormsModule]
 })
 export class AppComponent {
   title = 'Angular17';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent{
   public name = 'Huy Thế Phạm';
 
-  
+  public age = 22;
 
+
+  public resetName():void{
+    console.log("Reseted your name on input.")
+    this.name = '';
+  }
 }
