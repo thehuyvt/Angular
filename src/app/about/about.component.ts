99b8b95d4d8cf8f0a79902dbe6b484701a844d36
thehuyvt <thehuyvtvp@gmail.com>
2024-01-24 14:49:15 +0700

    Angular Switch Case

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgSwitch, NgSwitchCase, NgSwitchDefault, HomeComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  public loginName = 'admin';
 
 }
